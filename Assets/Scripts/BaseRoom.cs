d196ed83981dd2cfe352958efd913bc74e7a4b16
nmbryant <nmbryant@wpi.edu>
2014-10-30 20:30:16 -0400

    Adds ability to join a different room

---
+++ Assets/Scripts/BaseRoom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseRoom : MonoBehaviour {
+
+	private string name;
+	private string genre;
+	private int visits;
+	private int[] members;
+
+	// Use this for initialization
+	void Start () {
+
+		// Grab the room data from the database
+		name = "TestRoom";
+		genre = "TestGenre";
+		int visits = 1;
+		members = new int[10];
+		members [0] = 5;
+
+		// Increment visits and send to database
+		visits++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ElevatorMenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorMenu : MonoBehaviour {
+
+	private BaseRoom[] allRooms;
+	public GameObject currentRoom;
+	public GameObject roomTemplate;
+	public NetworkManager networkManager;
+	private bool isChangingRoom;
+
+	// Use this for initialization
+	void Start () {
+		isChangingRoom = false;
+		Connect ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Display the buttons for the elevator
+	void OnGUI() {
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,80,20), "Join Room")) {
+			isChangingRoom = true;
+			PhotonNetwork.LeaveRoom();
+
+			Destroy (currentRoom);
+			//Destroy (networkManager.myPlayerGO);
+
+			currentRoom = (GameObject) Instantiate(roomTemplate);
+		}
+	}
+
+	void Connect () {
+		PhotonNetwork.ConnectUsingSettings ("BefoHev V001");
+	}
+	
+	void OnJoinedLobby() {
+		Debug.Log ("BLAHBLAHBLAHBLAHBLAHB");
+		RoomOptions testRO = new RoomOptions ();
+		
+		// Join the room if it is already active on the server, otherwise create it
+		if (isChangingRoom){
+			PhotonNetwork.JoinOrCreateRoom ("name", testRO, PhotonNetwork.lobby);
+			isChangingRoom = false;
+		}
+	}
+}
