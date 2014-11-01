40023ee526454792d732ea10d9022805cae88ee8
Andrew Han <awhan@wpi.edu>
2014-10-31 20:11:40 -0400

    Pull rooms from server, generate GUI list of room buttons

--- Assets/Scripts/ElevatorMenu.cs
+++ Assets/Scripts/ElevatorMenu.cs
@@ -1,18 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
 
 public class ElevatorMenu : MonoBehaviour {
-
-	private BaseRoom[] allRooms;
+	
 	public GameObject currentRoom;
 	public GameObject roomTemplate;
 	public NetworkManager networkManager;
 	private bool isChangingRoom;
+	private RoomData[] allRooms = new RoomData[0];
+
+	private const string loginURL = "http://beforeheaveniqp.herokuapp.com/api/user/login";
+	private const string roomsURL = "http://beforeheaveniqp.herokuapp.com/api/rooms";
+
+	private string nextRoom;
+
+	public Vector2 scrollPosition = Vector2.zero;
+	public GUIStyle style;
 
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start () {
 		isChangingRoom = false;
-		Connect ();
+
+		WWWForm loginForm = new WWWForm ();
+		loginForm.AddField ("email", "awhan@wpi.edu");
+		loginForm.AddField ("password", "hiandy257");
+		WWW login = new WWW(loginURL, loginForm);
+		yield return login;
+		var parsed = JSON.Parse (login.text);
+
+		var headers = new Hashtable();
+		headers.Add ("Content-Type", "application/json");
+		headers.Add("X-User-Email", (parsed ["data"] ["email"]).ToString().Trim('"'));
+		headers.Add("X-User-Token", (parsed ["data"] ["authentication_token"]).ToString().Trim('"'));
+		WWW rooms = new WWW (roomsURL, null, headers);
+		yield return rooms;
+		Debug.Log (rooms.text);
+		var roomsParsed = JSON.Parse (rooms.text);
+		allRooms = new RoomData[roomsParsed["data"].AsArray.Count];
+		int roomCount = 0;
+		foreach(JSONNode data in roomsParsed["data"].AsArray){
+			RoomData roomData = new RoomData(data["name"].ToString(), data["genre"].ToString(), data["visits"].AsInt, null);
+			allRooms[roomCount] = roomData;
+			roomCount++;
+		}
 	}
 	
 	// Update is called once per frame
@@ -35,6 +66,26 @@ public class ElevatorMenu : MonoBehaviour {
 
 			currentRoom = (GameObject) Instantiate(roomTemplate);
 		}
+
+		GUI.skin.scrollView = style;
+		if(allRooms.Length > 0){
+			scrollPosition = GUI.BeginScrollView(new Rect(200, 200, 220, 100), scrollPosition, new Rect(0, 0, 220, 20*allRooms.Length));
+			for (int i = 0; i < allRooms.Length; i++){
+				if(GUI.Button(new Rect(0, 20*i, 220, 20), allRooms[i].Name)){
+					isChangingRoom = true;
+					PhotonNetwork.LeaveRoom();
+
+					nextRoom = allRooms[i].Name;
+
+					Destroy (currentRoom);
+					//Destroy (networkManager.myPlayerGO);
+					
+					currentRoom = (GameObject) Instantiate(roomTemplate);
+				}
+			}
+			GUI.EndScrollView();
+		}
+
 	}
 
 	void Connect () {
@@ -42,12 +93,11 @@ public class ElevatorMenu : MonoBehaviour {
 	}
 	
 	void OnJoinedLobby() {
-		Debug.Log ("BLAHBLAHBLAHBLAHBLAHB");
 		RoomOptions testRO = new RoomOptions ();
 		
 		// Join the room if it is already active on the server, otherwise create it
 		if (isChangingRoom){
-			PhotonNetwork.JoinOrCreateRoom ("name", testRO, PhotonNetwork.lobby);
+			PhotonNetwork.JoinOrCreateRoom (nextRoom, testRO, PhotonNetwork.lobby);
 			isChangingRoom = false;
 		}
 	}
---
+++ Assets/Scripts/RoomData.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomData{
+	private string name;
+	private string genre;
+	private int visits;
+	private int[] members;
+
+	public RoomData(string name, string genre, int visits, int[] members){
+		this.name = name;
+		this.genre = genre;
+		this.visits = visits;
+		this.members = members;
+	}
+
+	public string Name {
+		get {
+			return this.name;
+		}
+		set {
+			name = value;
+		}
+	}
+
+	public string Genre {
+		get {
+			return this.genre;
+		}
+		set {
+			genre = value;
+		}
+	}
+
+	public int Visits {
+		get {
+			return this.visits;
+		}
+		set {
+			visits = value;
+		}
+	}
+
+	public int[] Members {
+		get {
+			return this.members;
+		}
+		set {
+			members = value;
+		}
+	}
+
+}
