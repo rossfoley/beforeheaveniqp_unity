1cf276ffb72bf446e44d6cdcefd6b9c483d96d30
Sean MacEachern <ssmaceachern@wpi.edu>
2014-11-06 18:45:31 -0500

    Audio Streaming loads Current Song in a room.

--- Assets/Scripts/StreamAudioAndSetTime.cs
+++ Assets/Scripts/StreamAudioAndSetTime.cs
@@ -4,6 +4,7 @@ using System.IO;
 
 using NAudio;
 using NAudio.Wave;
+using SimpleJSON;
 
 public class StreamAudioAndSetTime : MonoBehaviour {
 	private IWavePlayer nWaveOutDevice;
@@ -30,8 +31,9 @@ public class StreamAudioAndSetTime : MonoBehaviour {
 		return false;
 	}
 
-	private void LoadAudio(){
-		WWW www = new WWW("https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3");
+	private void DebugLoadAudio(){
+		//WWW www = new WWW("https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3");
+		WWW www = new WWW("https://ec-media.soundcloud.com/g4Xt6e05MIcR.128.mp3?f10880d39085a94a0418a7ef69b03d522cd6dfee9399eeb9a52200996ffdb9349ddb954ba046f08712a18829fd04ed9574d7111478fe44bdfdfa96d2f7f5267555de5d6e28&AWSAccessKeyId=AKIAJNIGGLK7XA7YZSNQ&Expires=1415057882&Signature=0I%2F1rQNJwi7KZ0xyYbHb4Md%2B4CM%3D");
 		while(!www.isDone);
 		Debug.Log("URL Found");
 
@@ -44,46 +46,61 @@ public class StreamAudioAndSetTime : MonoBehaviour {
 		Resources.UnloadUnusedAssets();
 	}
 
-	public void LoginUser(string username, string enteredPassword){
-		WWWForm form = new WWWForm();
-		form.AddField("username", username);
-		form.AddField("password", enteredPassword);
-		byte[] rawData = form.data;
-
-		Hashtable headers = new Hashtable();
-		headers = form.headers;
-
-		string url = "http://beforeheaveniqp.herokuapp.com/";
-
-		headers["Authorization"] = "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes("username:password"));
-		WWW www = new WWW(url, form);
-
-		StartCoroutine(WaitForRequest(www));
+	private void LoadAudio(string input_url){
+		//WWW www = new WWW("https://api.soundcloud.com/tracks/174468803/stream?client_id=0cb45a6052596ee086177b11b29e8809");
+		WWW www = new WWW(input_url);
+		while(!www.isDone);
+		Debug.Log("URL Found");
+		
+		byte[] imageData = www.bytes;
+		if(!LoadAudioFromData(imageData)){
+			Debug.LogError("Couldn't load Audio bytes");
+		}
+		
+		nWaveOutDevice.Play();
+		Resources.UnloadUnusedAssets();
 	}
 
-	/*
-	 * string url = "http://beforeheaveniqp.herokuapp.com/user/login";
+	IEnumerator LoginUser(string username, string enteredPassword){
+		string url = "http://beforeheaveniqp.herokuapp.com/api/user/login";
+		string roomsUrl = "http://beforeheaveniqp.herokuapp.com/api/rooms";
+		string songUrl = "http://beforeheaveniqp.herokuapp.com/api/rooms/:room_id/current_song";
 
 		WWWForm form = new WWWForm();
-		form.AddField("username", username);
+		form.AddField("email", username);
 		form.AddField("password", enteredPassword);
-		WWW www = new WWW(url, form);
-		yield return www;
-		var parsed = JSON.Parse(www.text);
 
-		// Save the user email and auth key
+		WWW login = new WWW(url, form);
+
+		yield return login;
+		var parsed = JSON.Parse(login.text); Debug.Log(parsed);
+
+		//headers = form.headers;
+
 		string userEmail = (parsed ["data"] ["email"]).ToString().Trim('"');
 		string userAuthKey = (parsed ["data"] ["authentication_token"]).ToString ().Trim ('"');
+		Debug.Log(userEmail + "" + userAuthKey);
 
 		Hashtable headers = new Hashtable();
-		headers = form.headers;
-		
-		headers.Add ("Content-Type", "application/json");
+		headers.Add("Content-Type", "application/json");
 		headers.Add("X-User-Email", userEmail);
 		headers.Add("X-User-Token", userAuthKey);
-		//WWW mp3stream = new WWW (roomsURL, null, headers);
-		//yield return rooms;
-	 **/
+		WWW mp3stream = new WWW (roomsUrl, null, headers);
+
+		yield return mp3stream;
+
+		var mp3parsed = JSON.Parse(mp3stream.text); Debug.Log(mp3parsed);
+
+		string[] mp3link = new string[16];
+		int counter = 0;
+		foreach(JSONNode data in mp3parsed["data"].AsArray){
+			Debug.Log(data.ToString());
+			mp3link[counter] = data["current_song"].ToString();
+			counter++;
+		}
+		Debug.Log(mp3link[0]);
+		LoadAudio(mp3link[0].ToString().Trim('"'));
+	}
 
 	IEnumerator WaitForRequest(WWW www){
 		yield return www;
@@ -101,21 +118,7 @@ public class StreamAudioAndSetTime : MonoBehaviour {
 	void Start () {
 		o_username = "butts@ss.com";
 		o_password = "butts123";
-		LoginUser(o_username, o_password);
-		//LoadAudio();
-		/*
-		WWW www = new WWW ("http://localhost:8888/test2.ogg");  // start a download of the given URL
-		AudioClip clip  = www.GetAudioClip(false, true); // 2D, streaming
-		
-		while(!clip.isReadyToPlay)
-		{
-			Debug.Log("Waiting");
-		}
-		
-		audio.clip = clip;
-		audio.Play();
-		audio.time = 5; // skip to 5 seconds in
-		*/
+		StartCoroutine(LoginUser(o_username, o_password));
 	}
 	
 	// Update is called once per frame
