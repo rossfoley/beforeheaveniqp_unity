1f50804df72f28e48ad450415fdf1746f5d9c50d
Sean MacEachern <ssmaceachern@wpi.edu>
2014-11-11 19:55:11 -0500

    Current changes

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using SimpleJSON;
+
+public class AudioController : MonoBehaviour {
+	static AudioModel[] AudioList = new AudioModel[20];
+	static string RoomURL = "http://beforeheaveniqp.herokuapp.com/api/rooms";
+
+	public IEnumerator getSongData(){
+		Hashtable headers = new Hashtable();
+		headers.Add("Content-Type", "application/json");
+		headers.Add("X-User-Email", LoginModel.UserEmail);
+		headers.Add("X-User-Token", LoginModel.AuthKey);
+		WWW mp3stream = new WWW (roomURL, null, headers);
+		
+		yield return mp3stream;
+		var mp3parsed = JSON.Parse(mp3stream.text); 
+		Debug.Log(mp3parsed);
+		
+		WWW song_meta = new WWW (sampleRoom, null, headers);
+		yield return song_meta;
+		var song_meta_parsed = JSON.Parse(song_meta.text); 
+		Debug.Log(song_meta_parsed);
+		
+		int counter = 0;
+		foreach(JSONNode data in mp3parsed["data"].AsArray){
+			Debug.Log(data.ToString());
+			AudioList[counter].Url = data["current_song"].ToString().Trim('"');
+			counter++;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(getSongData());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/AudioModel.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioModel : MonoBehaviour {
+	string name;
+	string owner;
+	string genre;
+	string url;
+	int elapsed_time;
+
+	public AudioModel(string name, string owner, 
+	                  string genre, int elapsed_time){
+		this.name = name;
+		this.owner = owner;
+		this.genre = genre;
+		this.elapsed_time = elapsed_time;
+	}
+
+	public string Name {
+		get {
+			return name;
+		}
+		set {
+			name = value;
+		}
+	}
+
+	public string Owner {
+		get {
+			return owner;
+		}
+		set {
+			owner = value;
+		}
+	}
+
+	public string Genre {
+		get {
+			return genre;
+		}
+		set {
+			genre = value;
+		}
+	}
+
+	public string Url {
+		get {
+			return url;
+		}
+		set {
+			url = value;
+		}
+	}
+
+	public int Elapsed_time {
+		get {
+			return elapsed_time;
+		}
+		set {
+			elapsed_time = value;
+		}
+	}
+}
