d85295324ca6bdd0732d5a693283ab0130a4b245
nmbryant <nmbryant@wpi.edu>
2014-12-02 11:45:10 -0500

    Changes GUIController so that only one menu can appear on screen at a time

--- Assets/Scripts/controllers/GUIController.cs
+++ Assets/Scripts/controllers/GUIController.cs
@@ -13,6 +13,10 @@ public class GUIController : MonoBehaviour {
 	private static bool crWindowVisible = false; // Room config menu
 	private static bool flWindowVisible = false; // Friends list menu
 
+	private bool elMenuActive = false;
+	private bool crWindowActive = false;
+	private bool flWindowActive = false;
+
 	private Rect topBarRect = new Rect (guiEdgeBorder, guiEdgeBorder, 600, 60);
 
 	// Super awesome variable for distance between edges of things
@@ -45,30 +49,19 @@ public class GUIController : MonoBehaviour {
 	// Creates the top-level GUI
 	void OnGUI() {
 
-		// If the elevator window is visible, create the GUI window
-		if (elWindowVisible) {
-			elevatorMenu.SetActive(true);
-		}
-		else {
+		if (!elWindowVisible && elMenuActive){
 			elevatorMenu.SetActive(false);
+			elMenuActive = false;
 		}
-		
-		// If the create room window is visible, create + display all the GUI elements of the window
-		if (crWindowVisible) {
-			roomConfigMenu.SetActive(true);
-		}
-		else {
+		if (!crWindowVisible && crWindowActive){
 			roomConfigMenu.SetActive(false);
+			crWindowActive = false;
+		}
+		if (!flWindowVisible && flWindowActive){
+			friendsListMenu.SetActive(true);
+			flWindowActive = false;
 		}
 
-		// If the friends list window is visible, create the GUI window
-		if (flWindowVisible) {
-			friendsListMenu.SetActive(true);
-		}
-		else {
-			friendsListMenu.SetActive(false);
-		}
-		
 		GUILayout.BeginArea (topBarRect);
 		GUILayout.BeginHorizontal();
 		
@@ -79,17 +72,70 @@ public class GUIController : MonoBehaviour {
 				StartCoroutine(RoomController.getInstance().getRooms(""));
 			}
 			elWindowVisible = !elWindowVisible;
+			// If the elevator window is visible, create the GUI window
+			if (elWindowVisible) {
+				elevatorMenu.SetActive(true);
+				elMenuActive = true;
+				if (crWindowVisible || flWindowVisible){
+					crWindowVisible = false;
+					flWindowVisible = false;
+					roomConfigMenu.SetActive(false);
+					friendsListMenu.SetActive(false);
+					crWindowActive = false;
+					flWindowActive = false;
+				}
+				Debug.Log("Activating elevator menu");
+			}
+			else {
+				elevatorMenu.SetActive(false);
+				elMenuActive = false;
+			}
 		}
 
 		// Room Edit / Room Create top-level GUI button
 		// TODO: Make the button change on whether or not user owns room
 		if (GUILayout.Button ("Create Room")) {
 			crWindowVisible = !crWindowVisible;
+			// If the create room window is visible, create + display all the GUI elements of the window
+			if (crWindowVisible) {
+				roomConfigMenu.SetActive(true);
+				crWindowActive = true;
+				if (elWindowVisible || flWindowVisible){
+					elWindowVisible = false;
+					flWindowVisible = false;
+					elevatorMenu.SetActive(false);
+					friendsListMenu.SetActive(false);
+					elMenuActive = false;
+					flWindowActive = false;
+				}
+			}
+			else {
+				roomConfigMenu.SetActive(false);
+				crWindowActive = false;
+			}
 		}
 
 		// Friend list top-level GUI button
 		if (GUILayout.Button ("Friends List")) {
 			flWindowVisible = !flWindowVisible;
+			// If the friends list window is visible, create the GUI window
+			if (flWindowVisible) {
+				friendsListMenu.SetActive(true);
+				flWindowActive = true;
+				if (crWindowVisible || elWindowVisible){
+					crWindowVisible = false;
+					elWindowVisible = false;
+					crWindowActive = false;
+					elMenuActive = false;
+					roomConfigMenu.SetActive(false);
+					elevatorMenu.SetActive(false);
+				}
+				Debug.Log("Activating friends list");
+			}
+			else {
+				friendsListMenu.SetActive(false);
+				flWindowActive = false;
+			}
 		}
 		
 		GUILayout.EndHorizontal();
